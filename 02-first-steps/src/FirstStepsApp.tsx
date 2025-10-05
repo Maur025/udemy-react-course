de29e9c7e6ce585ea24e612e7d8fe997b0ba18af
maur025 <mmoyapalli@gmail.com>
2025-10-05 11:15:39 -0400

    init react course repository

---
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -0,0 +1,19 @@
+import { MyAwesomeApp } from "./MyAwesomeApp";
+
+export function FirstStepsApp() {
+	return (
+		<>
+			<h1>Hola Mundo!!</h1>
+
+			<p>esto es un párrafo!!</p>
+
+			<button>click me</button>
+
+			<div>
+				<h2>Hola dentro de un div</h2>
+			</div>
+
+			<MyAwesomeApp />
+		</>
+	);
+}
